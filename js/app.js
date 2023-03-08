ae55154f1799c9451893ee40fca98cb03e8e2818
Ismoiljon Abdulloev <110225249+abdulloev19@users.noreply.github.com>
2023-03-08 15:05:26 +0500

    updated

--- js/app.js
+++ js/app.js
@@ -7,6 +7,7 @@ cls.addEventListener('click', () => {
     tg.close()
 })
 username.innerHTML = tg.initDataUnsafe?.user?.username;
+username.innerHTML += tg.initDataUnsafe?.user?.first_name
 
 toggle.addEvenListener('click',function(){
    if(tg.MainButton.isVisible){
