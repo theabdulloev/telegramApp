c6c9fd62565bc0c4d9f3895d075a04f74f7ddfce
Ismoiljon Abdulloev <110225249+abdulloev19@users.noreply.github.com>
2023-03-08 16:13:16 +0500

    eeee33

--- js/app.js
+++ js/app.js
@@ -9,7 +9,7 @@ cls.addEventListener('click', () => {
 username.innerHTML = tg.initDataUnsafe?.user?.username;
 username.innerHTML += JSON.stringify(tg)
 
-
+tg.openLink('instagramtj.netlify.app')
 tg.showAlert('Привет')
 
 toggle.addEvenListener('click',function(){
